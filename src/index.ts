2831b1115ceb523104614e6d7668d608b359d257
leovs09 <leovs010@gmail.com>
2026-03-02 16:19:09 +0100

    feat: export util functions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './log.decorator';
 export * from './types';
 export * from './LogWrapper';
 export * from './axios/axios.logger';
+export * from './decorate/applyToMethod';
+export * from './decorate/applyToClass';
