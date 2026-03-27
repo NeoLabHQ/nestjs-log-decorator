48ba1cde53ade0fbcdb95b5e235a60fea6d76a96
leovs09 <leovs010@gmail.com>
2026-03-27 23:30:44 +0100

    feat: add isTimoutError util for axios logging

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './log.decorator';
 export * from './types';
 export * from './LogWrapper';
 export * from './axios/axios.logger';
+export * from './axios/isTimoutError';
 export * from './decorate/applyToMethod';
 export * from './decorate/applyToClass';
